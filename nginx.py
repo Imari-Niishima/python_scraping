1b4a8294baa39a16c9938a811d90dc9b52e5eb54
unknown <tmrm.ryo45@gmail.com>
2024-11-17 23:01:55 +0900

    message

--- nginx.py
+++ nginx.py
@@ -1,5 +1,5 @@
-#Apache.py 2024/11/4 by Imari
-#Apacheのバージョンを取得するプログラム
+#nginx.py 2024/11/17 by Imari
+#nginxのバージョンを取得するプログラム
 #今回読んだバージョンか、引数にした以前取ったバージョンのどっちかを文字列で返す
 #それをバージョン管理のテキストに書き込むようにmain側でいじる
 #動作確認のためにメイン関数にしてるけど使うときは関数の方に直すこと。
@@ -16,27 +16,44 @@ import sys
 from bs4 import BeautifulSoup
 
 
-#def version_check(apache_version):
+#def version_check(nginx_version):
 if __name__ == "__main__":
-    url_base = "https://httpd.apache.org/"
+    url_base = "https://nginx.org"
 
     html_base = requests.get(url_base).text
 
     soup_base = BeautifulSoup(html_base, "html5lib")
 
-    for a_each in soup_base.find_all("h1"):
+    news_table = soup_base.find_all("table", class_="news")[0]
+
+   
+    for a_each in news_table.find_all("tr"):
         text_cand = a_each.get_text()
 
+        print("****\n")
         print(text_cand)
-        #sys.exit()
 
+        #nginx-***とか2024-****で情報を取得して更新
+        #mainlineという記述の有無でフラグ管理しよう
+
+        #none_check = re.search("Latest News", text_cand)
+
+        now_version = re.findall("nginx-(.*)\n", text_cand)
+        print("now_version:", now_version)
+
+        mainline_judge = re.findall("mainline", text_cand)
+        print("judge:", mainline_judge)
+
+        if (len(mainline_judge) != 0) and (len(now_version) != 0):
+            print("ok!\n")
         
 
-        none_check = re.search("Released", text_cand)
-
+        
+        #sys.exit()
+        """
         if (none_check is not None) == True:
 
-            #now_version = re.findall("httpd (.*) Released", apache_version)
+            #pre_version = re.findall("httpd (.*) Released", apache_version)
             pre_version = re.findall("httpd (.*) Released", "Apache httpd 2.2.12 Released 2022-07-17")
             now_version = re.findall("httpd (.*) Released", text_cand)
             
@@ -49,7 +66,7 @@ if __name__ == "__main__":
                     print("version apdated!\n")
                     print(text_cand.rstrip("¶"))
                     #return text_cand.rstrip("¶")
-
+        """
     #return apache_version
             
 
